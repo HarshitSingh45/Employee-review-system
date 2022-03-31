4678b6c645f947c17a7169990a4e5a1a76ebca83
Harshit Singh <harshitsingh@Ankurs-MacBook-Air.local>
2022-03-31 13:24:08 +0530

    admin can update reviewed reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -8,3 +8,19 @@ module.exports.submitReview = async(req, res) => {
     review.save();
     return res.redirect('/');
 }
+
+module.exports.updatePage = async(req, res) => {
+    let review = await Reviews.findById(req.params.id).populate('reviewOf');
+    return res.render('updatePR', {
+        review
+    });
+}
+module.exports.update = async(req, res) => {
+    let review = await Reviews.findById(req.params.id);
+    review.friendly = req.body.friendly;
+    review.cooperative = req.body.cooperative;
+    review.responsible = req.body.responsible;
+    review.reviewBy = req.user._id;
+    review.save();
+    return res.redirect('/assign/allPR');
+}
--- routes/review.js
+++ routes/review.js
@@ -4,5 +4,7 @@ const reviewController = require('../controllers/reviewController');
 const passport = require('passport');
 
 router.post('/submit/:id', passport.checkAuthentication, reviewController.submitReview);
+router.get('/updatePage/:id', passport.checkAuthentication, reviewController.updatePage);
+router.post('/update/:id', passport.checkAuthentication, reviewController.update);
 
 module.exports = router;
